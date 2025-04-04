c5b68cee84106702181c4ad6e316344296364bab
nikhil <nikhilmehra9826@gmail.com>
2025-04-04 15:11:05 +0530

    slack channel change

--- src/config.py
+++ src/config.py
@@ -12,7 +12,7 @@ GEMINI_ENDPOINT = "https://generativelanguage.googleapis.com/v1beta/models/gemin
 
 # Slack API key (if integrating)
 SLACK_token = 'xoxb-8698313086771-8698027728964-MLgSg2KdE95DPQzdW5VkxRXD'
-channel="C08L111TK1D"
+channel="C08LU6MHC20"
 # Calendar API settings could go here as well
 #till now not work on there
 # Database configuration (using SQLite for simplicity)
